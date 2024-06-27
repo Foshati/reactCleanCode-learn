e3e637fbf9652d886362ccc08af1ef5e980ba5b1
Foshati <amirrezafoshati@gmail.com>
2024-06-27 15:08:49 +0330

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {}, []);
+  useEffect(() => {}, []);
 
   return (
     <>
+
+
+
+
+    
       <div>
         <a href='https://vitejs.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
